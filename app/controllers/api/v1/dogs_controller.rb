4e003e90f226edd85a61fa05fa7902368278fead
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-06 14:44:22 +0300

    Update created the index method to show all dogs

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController Api::V1::ApplicationController 
+  def index
+    dogs = Dog.all 
+    if dogs
+      render json: {status: "SUCCESS", data: dogs, message: "Dogs were loaded succesfully!"}, status: :ok
+    else
+      render json: dogs.errors, status: :bad_request
+    end
+  end
 end

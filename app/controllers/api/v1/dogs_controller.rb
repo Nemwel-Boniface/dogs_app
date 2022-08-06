d3458114696c4446b6c1618cfbce9f52141d0595
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-06 14:45:07 +0300

    Update created the show method to show each individual dog

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -7,4 +7,13 @@ class DogsController Api::V1::ApplicationController
       render json: dogs.errors, status: :bad_request
     end
   end
+
+  def show 
+    dog = Dog.find(params[:id])
+    if dog
+      render json: {data: dog}, status: :ok
+    else
+      render json: {message: "Dog cannot be found!"}, status: :bad_request
+    end
+  end
 end
